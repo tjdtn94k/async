24f3c5f2c09c5c74f5f06ed8b73eb8645586c7ed
kimseongsu <ss.kim@okestro.com>
2024-03-18 13:19:14 +0900

    first commit

---
+++ src/main/java/dev/backend/prac_aysnc/service/AsyncService.java
@@ -0,0 +1,45 @@
+package dev.backend.prac_aysnc.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AsyncService {
+
+    private final EmailService emailService;
+
+    /**
+     * 비동기 동작 O
+     */
+    public void asyncCall_1() {
+        System.out.println("[asyncCall_1] :: " + Thread.currentThread().getName());
+        emailService.sendMail();
+        emailService.sendMailWithCustomThreadPool();
+    }
+
+    /**
+     * 비동기 동작 X
+     */
+    public void asyncCall_2() {
+        System.out.println("[asyncCall_2] :: " + Thread.currentThread().getName());
+        EmailService emailService = new EmailService();
+        emailService.sendMail();
+        emailService.sendMailWithCustomThreadPool();
+    }
+
+    /**
+     * 비동기 동작 X
+     */
+    public void asyncCall_3() {
+        System.out.println("[asyncCall_3] :: " + Thread.currentThread().getName());
+        sendMail();
+    }
+
+    @Async
+    public void sendMail() {
+        System.out.println("[sendMail] :: " + Thread.currentThread().getName());
+    }
+}
